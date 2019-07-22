11221f6a66e88871b753f78a1332d4cfa33680b8
Lanh Nguyen <nguyenkimlanh2010@gmail.com>
2019-07-23 02:12:19 +0700

    first page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+let background = require('./images/Main1.png');
 
+let styles = {
+  root: {
+    backgroundImage: 'url(' + background + ')',
+    backgroundSize: 'cover',
+    height: '100%',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat'
+  }
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={styles.root}>
+      <button className="button">Tìm Hiểu Ngay</button>
     </div>
   );
 }
