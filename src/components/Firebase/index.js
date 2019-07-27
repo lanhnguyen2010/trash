fa2d27f79dcc1d1a239f2658be9b3c1ab843b6c7
Lanh Nguyen <nguyenkimlanh2010@gmail.com>
2019-07-27 15:40:09 +0700

    change firebase app

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -2,13 +2,13 @@ import * as firebase from 'firebase';
 import ReduxSagaFirebase from 'redux-saga-firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAx7SCZvR4lo6ovKEK_m97M-OhrW4Spnn0",
-  authDomain: "myfirstapp-8e0d6.firebaseapp.com",
-  databaseURL: "https://myfirstapp-8e0d6.firebaseio.com",
-  projectId: "myfirstapp-8e0d6",
-  storageBucket: "myfirstapp-8e0d6.appspot.com",
-  messagingSenderId: "246619864806",
-  appId: "1:246619864806:web:20fbbc79603c7e9f"
+  apiKey: "AIzaSyBOFGzQIrAmR_npvTVgtYLNMAGXrC1ryZE",
+  authDomain: "prugood-7a99a.firebaseapp.com",
+  databaseURL: "https://prugood-7a99a.firebaseio.com",
+  projectId: "prugood-7a99a",
+  storageBucket: "",
+  messagingSenderId: "319012787187",
+  appId: "1:319012787187:web:3e609a05635aa231"
 });
 
 const firebaseService = new ReduxSagaFirebase(firebaseApp);
