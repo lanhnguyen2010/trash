ea88d44b83b195a87c84d0c5de901953e713e04e
Hung Dao <hungdao1311@gmail.com>
2019-07-27 13:21:48 +0700

    Init homepage UI

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import * as ROUTES from "../../constants/routes";
 
-import {Button} from '@material-ui/core';
+import {Button, Container, Fab} from '@material-ui/core';
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 import { Link } from "react-router-dom";
@@ -14,7 +14,9 @@ const styles = {
     zIndex: 100,
     display: "flex",
     justifyContent: "center",
-    width: "100%"
+    width: "100%",
+    height: "20%",
+    backgroundColor: "white",
   }
 };
 
@@ -33,23 +35,26 @@ export default class Home extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div style={styles.footer}>
-          <Button style={{minWidth: "70%", alignSelf: "center"}} onClick={() => this.routeChange()}>
-            Tim hieu ngay</Button>
-        </div>
+      <Container>
         <AwesomeSlider style={{width: '100%', height:'100%'}}
                        bullets={false}
                        infinite={true}
                        organicArrows={false}
                        ref={t => this.track = t}>
-          <div style={{backgroundColor: '#2d5182'}}>1</div>
+          <div><img src="./images/Home1.svg"/></div>
           <div style={{backgroundColor: '#5fb7b2'}}>2</div>
           <div style={{backgroundColor: '#fcd0a8'}}>3</div>
-          <div style={{backgroundColor: 'red'}}>4</div>
+          <div><img src="./images/Home1.svg"/></div>
         </AwesomeSlider>
-
-      </div>
+        <div style={styles.footer}>
+          <Fab style={{ alignSelf: "center", backgroundColor: '#D20C08', width: "20%"}} 
+                  variant='extended'
+                  color='primary'
+                  size='large'
+                  onClick={() => this.routeChange()}>
+            Tìm hiểu thêm</Fab>
+        </div> 
+      </Container>
     );
   }
 }
