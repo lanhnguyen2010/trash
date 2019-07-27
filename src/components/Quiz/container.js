917829a1e81211ad083d4b9b09bda59955f3f834
TrangDao <trang020192@gmail.com>
2019-07-27 15:17:51 +0700

    first question

--- src/components/Quiz/container.js
+++ src/components/Quiz/container.js
@@ -5,6 +5,7 @@ import {selectors, actions} from "../../redux";
 import {lifecycle} from "recompose";
 import Quiz from "./component"
 
+
 const QuizContainer = compose(
   connect(
     selectors.root,
@@ -14,9 +15,22 @@ const QuizContainer = compose(
   ),
   lifecycle({
     componentDidMount() {
-      setTimeout(function() { //Start the timer
-        this.setState({render: true}) ;
-      }.bind(this), 3000)
+      const btnStateInit = [false, false, false, false];
+      const initState = {
+        render: false,
+        btnState: btnStateInit,
+        toogleState: (index) => {
+          btnStateInit[index] = true;
+          this.setState({...this.props.state, btnState: btnStateInit, toogleState: (index) => {} });
+          console.log(this);
+        }
+      };
+
+      this.setState(initState);
+
+      setTimeout(function() {
+        this.setState({...initState, render:true}) ;
+      }.bind(this), 1000)
     }
   })
 )(Quiz);
