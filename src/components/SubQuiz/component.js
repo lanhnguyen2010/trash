66dd056c16306de0c10191fd9350a8b98e889566
Lanh Nguyen <nguyenkimlanh2010@gmail.com>
2019-07-29 19:19:55 +0700

    sub quiz

--- src/components/SubQuiz/component.js
+++ src/components/SubQuiz/component.js
@@ -22,11 +22,12 @@ const styles = {
 
   questionContent: {
     fontSize: '3vh',
+    textAlign:'justify'
   },
 
-  answer:{
+  answer: {
     margin: 'auto',
-    width:'70%'
+    width: '70%'
   },
 
   baseAnswer: {
@@ -93,27 +94,45 @@ const styles = {
   },
 };
 
+function getStyle(label) {
+  if (label == "hộp nhựa") {
+    return {...styles.questionSection, ...styles.backgroundHop, ...commonStyles.container}
+  }
+  else if (label == "chai nhựa") {
+    return {...styles.questionSection, ...styles.backgroundChaiNhua, ...commonStyles.container}
+
+  }
+  else if (label == "ly nhựa") {
+    return {...styles.questionSection, ...styles.backgroundLyNhua, ...commonStyles.container}
+
+  }
+  else {
+    return {...styles.questionSection, ...styles.backgroundOther, ...commonStyles.container}
+  }
+}
+
 const SubQuiz = ({history, answerResult, btnState, toogleState, selectedTrash, question}) => {
     return (
       <div className="container" style={{display: 'flex', alignItems: 'stretch', alignContent: 'stretch'}}>
-        {answerResult ? <Result {...{history, answerResult}}/> :
+        {answerResult ? <Result {...{history, answerResult, question}}/> :
           <Question {...{btnState, toogleState, selectedTrash, question}}/>}
       </div>
     )
   }
 ;
 
-const Result = ({history, answerResult}) => (
-  <div>
-    <div>
-      {answerResult ? "Bạn đã chọn chính xac" : "Câu trả lời chưa đúng"}
-    </div>
+const Result = ({history, answerResult, question}) => (
+  <div style={getStyle(question.label)}>\
+    <div style={styles.questionTitle}>
+      <div style={styles.questionLabel}>
+        {answerResult ? "Bạn đã chọn chính xác" : "Câu trả lời chưa đúng"}
+      </div>
 
-    <div>
-      Cái giá thật sự phải trả cho việc sử dụng nhựa thật sự đắt hơn chúng ta biết đấy.
+      <div style={styles.questionContent}>
+        Cái giá thật sự phải trả cho việc sử dụng nhựa thật sự đắt hơn chúng ta biết đấy.
+      </div>
     </div>
-
-    <button onClick={() => history.push(Routes.MORE_INFO)}>Tìm hiểu thêm về cái giá thật sự của nhựa</button>
+    <Button style={commonStyles.button} onClick={() => history.push(Routes.MORE_INFO)}>Tìm hiểu thêm về cái giá thật sự của nhựa</Button>
   </div>
 );
 
@@ -126,22 +145,6 @@ const QuestionStyle = {
 
 const Question = ({btnState, toogleState, selectedTrash, question}) => {
 
-  function getStyle(label) {
-    if (label == "hộp nhựa") {
-      return {...styles.questionSection, ...styles.backgroundHop, ...commonStyles.container}
-    }
-    else if (label == "chai nhựa") {
-      return {...styles.questionSection, ...styles.backgroundChaiNhua, ...commonStyles.container}
-
-    }
-    else if (label == "ly nhựa") {
-      return {...styles.questionSection, ...styles.backgroundLyNhua, ...commonStyles.container}
-
-    }
-    else {
-      return {...styles.questionSection, ...styles.backgroundOther, ...commonStyles.container}
-    }
-  }
 
   return question ? (
     <div style={getStyle(question.label)}>
